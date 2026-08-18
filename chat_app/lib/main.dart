4eede9dc6c9cdc16bc9e1e6b9c9330c02024c96f
Robert Impey <robert-impey@users.noreply.github.com>
2026-08-18 09:36:31 -0700

    Added some components to the widget

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -12,14 +12,26 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello, World!"),
-        ),
-      ),
+      title: 'My Material App',
+      home: LoginPageWidget(),
     );
   }
-  
 }
 
 // TODO Create LoginPage widget
+class LoginPageWidget extends StatelessWidget {
+  const LoginPageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Bar Title'),
+      ),
+      drawer: const Drawer(),
+      body: const Center(
+        child: Text("Hello, World!"),
+      ),
+    );
+  }
+}
