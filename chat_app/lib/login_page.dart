e1e1473b2473fd620a381c21a03b28ec30f45082
Robert Impey <robert-impey@users.noreply.github.com>
2026-08-24 21:46:15 -0700

    More text in the column

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -6,17 +6,28 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: const Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
-      ),
-      //TODO: Add the text and image from the design
-      body: const Text('Let\'s sign you in',
-          style: TextStyle(fontSize: 30, color: Colors.brown,
-          fontWeight: FontWeight.bold, letterSpacing: 0.75)),
-    );
+        appBar: AppBar(),
+        drawer: const Drawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Button clicked');
+          },
+        ),
+        //TODO: Add the text and image from the design
+        body: const Column(
+          children: [
+            Text('Let\'s sign you in',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.brown,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.75)),
+            Text('Welcome back!\nYou\'ve been missed!',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.blueGrey)),
+          ],
+        ));
   }
 }
