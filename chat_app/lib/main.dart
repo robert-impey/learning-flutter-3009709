bf97a3741fc0f01ff393af0ea01a59bedabbc6c3
Robert Impey <robert-impey@users.noreply.github.com>
2026-09-04 15:08:19 +0100

    Dart fix and updated the tests to avoid HTTP

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -2,17 +2,19 @@ import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ChatApp());
+  runApp(const ChatApp());
 }
 
 class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Chat App",
       theme: ThemeData(primarySwatch: Colors.yellow),
       //TODO: Create ChatPage() & add appBar, basic ChatBubble & ChatInput widget
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
--- chat_app/test/widget_test.dart
+++ chat_app/test/widget_test.dart
@@ -5,14 +5,77 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:chat_app/main.dart';
 
 void main() {
+  // Mocking HttpClient to avoid 400 errors in tests
+  setUpAll(() {
+    HttpOverrides.global = _MockHttpOverrides();
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ChatApp());
+    await tester.pumpWidget(const ChatApp());
   });
 }
+
+class _MockHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return _MockHttpClient();
+  }
+}
+
+class _MockHttpClient implements HttpClient {
+  @override
+  Future<HttpClientRequest> getUrl(Uri url) => Future.value(_MockHttpClientRequest());
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
+
+class _MockHttpClientRequest implements HttpClientRequest {
+  @override
+  Future<HttpClientResponse> close() => Future.value(_MockHttpClientResponse());
+
+  @override
+  HttpHeaders get headers => _MockHttpHeaders();
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
+
+class _MockHttpClientResponse extends Stream<List<int>> implements HttpClientResponse {
+  @override
+  StreamSubscription<List<int>> listen(void Function(List<int> event)? onData, {Function? onError, void Function()? onDone, bool? cancelOnError}) {
+    return Stream<List<int>>.fromIterable([_transparentImage]).listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+  }
+
+  @override
+  int get statusCode => 200;
+
+  @override
+  int get contentLength => _transparentImage.length;
+
+  @override
+  HttpClientResponseCompressionState get compressionState => HttpClientResponseCompressionState.notCompressed;
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
+
+class _MockHttpHeaders implements HttpHeaders {
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
+
+final List<int> _transparentImage = [
+  0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52,
+  0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4,
+  0x89, 0x00, 0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00,
+  0x05, 0x00, 0x01, 0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE,
+  0x42, 0x60, 0x82,
+];
