999f75a171426af84313fb45fed2e007d0a900e0
Robert Impey <robert-impey@users.noreply.github.com>
2026-09-14 15:54:46 +0100

    Fix image loading on macOS and iOS but not web

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -7,46 +7,51 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       //TODO: Align Text and image to the vertical & horizontal centre of the device.
-      body: Column(
-        children: [
-          const Text(
-            'Let\'s sign you in!',
-            style: TextStyle(
-                fontSize: 30,
-                color: Colors.brown,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.5),
-          ),
-
-          //TODO: Align text to centre
-          const Text(
-            'Welcome back! \n You\'ve been missed!',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.blueGrey),
-          ),
-          Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-              webHtmlElementStrategy: WebHtmlElementStrategy.prefer),
-          Container(
-            height: 150,
-            width: 150,
-            // child: FlutterLogo(),
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-                image: const DecorationImage(
-                  fit: BoxFit.fitHeight,
-                  image: NetworkImage(
-                      'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                      webHtmlElementStrategy: WebHtmlElementStrategy.prefer),
-                ),
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(24)),
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text(
+              'Let\'s sign you in!',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.brown,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5),
+            ),
+  
+            //TODO: Align text to centre
+            const Text(
+              'Welcome back! \n You\'ve been missed!',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.blueGrey),
+            ),
+            Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+                webHtmlElementStrategy: WebHtmlElementStrategy.prefer),
+            Container(
+              height: 150,
+              width: 150,
+              // child: FlutterLogo(),
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    fit: BoxFit.fitHeight,
+                    image: NetworkImage(
+                        'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                        webHtmlElementStrategy: WebHtmlElementStrategy.prefer),
+                  ),
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(24)),
+              child: const Center(
+                child: Text('Logo'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
