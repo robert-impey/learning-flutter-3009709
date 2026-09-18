700393d3a3a8047ea444ae057611aa25263b6d3a
Robert Impey <robert-impey@users.noreply.github.com>
2026-09-18 20:37:50 +0100

    Fix images in web

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -20,12 +20,44 @@ class ChatPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-
           //TODO: Remove duplicated code
           Align(
             alignment: Alignment.centerLeft,
             child: Container(
+                padding: const EdgeInsets.all(24),
+                margin: const EdgeInsets.all(50),
+                decoration: const BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                        bottomLeft: Radius.circular(12))),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Hi, this is your message!',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    Image.network(
+                      'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                      height: 200,
+                      webHtmlElementStrategy: WebHtmlElementStrategy.prefer,
+                    )
+                  ],
+                )),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
               padding: const EdgeInsets.all(24),
+              margin: const EdgeInsets.all(50),
+              decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -36,22 +68,23 @@ class ChatPage extends StatelessWidget {
                   Image.network(
                     'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
                     height: 200,
+                    webHtmlElementStrategy: WebHtmlElementStrategy.prefer,
                   )
                 ],
               ),
-              margin: const EdgeInsets.all(50),
-              decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                      bottomLeft: Radius.circular(12))),
             ),
           ),
           Align(
             alignment: Alignment.centerRight,
             child: Container(
               padding: const EdgeInsets.all(24),
+              margin: const EdgeInsets.all(50),
+              decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -62,48 +95,19 @@ class ChatPage extends StatelessWidget {
                   Image.network(
                     'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
                     height: 200,
+                    webHtmlElementStrategy: WebHtmlElementStrategy.prefer,
                   )
                 ],
               ),
-              margin: const EdgeInsets.all(50),
-              decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                      bottomLeft: Radius.circular(12))),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    'Hi, this is your message!',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  Image.network(
-                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                    height: 200,
-                  )
-                ],
-              ),
-              margin: const EdgeInsets.all(50),
-              decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                      bottomLeft: Radius.circular(12))),
             ),
           ),
 
           //TODO: Make it sticky
           Container(
             height: 100,
+            decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -123,9 +127,6 @@ class ChatPage extends StatelessWidget {
                 ),
               ],
             ),
-            decoration: const BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
           )
         ],
       ),
